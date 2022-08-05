d9b398a39251d906f75d3cca5ba19656653b8cfb
NoOseGG <hoc751@gmail.com>
2022-08-05 12:57:17 +0300

    added library androidx navigation added navigation graph (nav_graph)

--- app/src/main/java/com/example/personalnotebook/screen/LoginFragment.kt
+++ app/src/main/java/com/example/personalnotebook/screen/LoginFragment.kt
@@ -1,7 +1,33 @@
 package com.example.personalnotebook.screen
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.personalnotebook.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
 
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = requireNotNull(_binding)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return FragmentLoginBinding.inflate(inflater, container, false)
+            .also { _binding = it }
+            .root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
